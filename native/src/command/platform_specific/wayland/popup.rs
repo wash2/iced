1b12087094255959d402f19399d9d7fd1513a8c1
Ashley Wulber <ashley@system76.com>
2022-10-22 19:23:27 -0400

    popup

--- native/src/command/platform_specific/wayland/popup.rs
+++ native/src/command/platform_specific/wayland/popup.rs
@@ -3,14 +3,40 @@ use std::hash::{Hash, Hasher};
 use std::marker::PhantomData;
 use std::{collections::hash_map::DefaultHasher, fmt};
 
+use iced_core::Rectangle;
 use iced_futures::MaybeSend;
-use sctk::{
-    reexports::client::backend::ObjectId,
-};
+use sctk::reexports::protocols::xdg::shell::client::xdg_positioner::{Anchor, Gravity};
+use sctk::shell::xdg::XdgPositioner;
 
+
+
+use crate::window;
 /// TODO
 #[derive(Debug, Clone)]
-pub struct IcedPopup;
+pub struct IcedPopup{
+    /// XXX must be unique, id of the parent
+    parent: window::Id,
+    /// XXX must be unique, id of the popup
+    id: window::Id,
+    /// size of the popup
+    size: (u32, u32),
+    /// the rectangle which the popup will be anchored to
+    anchor_rect: Rectangle<u32>,
+    /// the anchor location on the popup
+    anchor: Anchor,
+    /// the gravity of the popup
+    gravity: Gravity,
+    /// the constraint adjustment
+    constraint_adjustment: u32,
+    /// offset of the popup
+    offset: (u32, u32),
+    /// whether the popup is reactive
+    reactive: bool,
+    /// optional parent size, must be correct or the behavior is undefined
+    parent_size: Option<(u32, u32)>,
+    /// whether a grab should be requested for the popup after creation
+    grab: bool
+}
 
 #[derive(Clone)]
 /// Window Action
